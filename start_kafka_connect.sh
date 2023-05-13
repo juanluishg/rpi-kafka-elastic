b9fe0d202cd2a482a9c821a46c41958c52535059
JuanLu <juanluishg@hotmail.es>
2023-05-13 20:59:16 +0200

    change topic name

--- start_kafka_connect.sh
+++ start_kafka_connect.sh
@@ -4,7 +4,7 @@ curl -X POST http://192.168.178.100:8083/connectors -H 'Content-Type: applicatio
   "config": {
     "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
     "tasks.max": "1",
-    "topics": "rpi-monitoring",
+    "topics": "internal-monitoring",
     "key.ignore": "true",
     "schema.ignore": "true",
     "connection.url": "http://elasticsearch:9200",
